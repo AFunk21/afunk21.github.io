55eb11ae67f0e04f545618a8a97feb234f54f4bf
fun09002@byui.edu <fun09002@byui.edu>
2021-04-23 14:14:17 -0500

    litteral commit

---
+++ js/index.js
@@ -0,0 +1,37 @@
+document.querySelector('#lastmod').textContent = document.lastModified;
+  
+
+  let daynames = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+  ]
+  
+  let months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'October',
+    'November',
+    'December'
+  ]
+  
+  let d = new Date();
+  
+  let dayName = daynames[d.getDay()];
+  let monthName = months[d.getMonth()];
+  let year = d.getFullYear();
+  let fulldate = 
+  
+  const modified = document.lastModified
+  
+  document.getElementById('currentdate').textContent = modified;
