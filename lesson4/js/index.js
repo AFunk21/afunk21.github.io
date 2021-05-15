d5f34ea11f138892cb240502cd362f56946e6821
fun09002@byui.edu <fun09002@byui.edu>
2021-05-15 12:37:46 -0500

    litteral commit

--- lesson4/js/index.js
+++ lesson4/js/index.js
@@ -1,2 +1,10 @@
 document.querySelector('#year').textContent = new Date().getFullYear();
 document.querySelector('#lastmod').textContent = document.lastModified;
+
+
+const hambutton = document.querySelector(".ham");
+hambutton.addEventListener("click", toggleMenu, false);
+
+function toggleMenu() {
+  document.querySelector("nav").classList.toggle("responsive");
+}
