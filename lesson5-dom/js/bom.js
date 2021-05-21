980e09227b4b2eaadd9dd6c76b1e28d4f445a6f2
fun09002@byui.edu <fun09002@byui.edu>
2021-05-20 20:14:28 -0500

    litteral commit

---
+++ lesson5-dom/js/bom.js
@@ -0,0 +1,27 @@
+const list = document.querySelector('ul');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+
+
+button.addEventListener("click", function() {
+    
+    let Item = input.value;
+    input.value = '';
+
+    const textList = document.createElement('span');
+    const listItem = document.createElement('li');
+    const buttonList = document.createElement('button');
+    
+    textList.textContent = Item;
+    buttonList.textContent = '❌';
+
+    listItem.append(textList);
+    listItem.appendChild(buttonList);
+    list.appendChild(listItem);
+
+    buttonList.addEventListener('click', function(e) {
+        list.removeChild(listItem);
+    });
+
+    input.focus();
+});
