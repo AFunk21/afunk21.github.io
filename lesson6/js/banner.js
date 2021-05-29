2fc8f716058e0d2199c3b8c25646634d357bf906
fun09002@byui.edu <fun09002@byui.edu>
2021-05-29 17:47:17 -0500

    litteral commit

--- lesson6/js/banner.js
+++ lesson6/js/banner.js
@@ -1,7 +1,6 @@
 let thedate = new Date()
 console.log(thedate.getDate())
 
-
 if (thedate.getDay() == 5) {
     document.querySelector('#banner').style.display = 'block';
 }
---
+++ lesson6/js/webfont.js
@@ -0,0 +1,6 @@
+WebFont.load({
+    google: {
+      families: 
+        ['Roboto, Audiowide, Bree+Serif, Comfortaa:wght@600, Courgette, Girassol, Lobster, Rubik:wght@300;400, Russo+One, Suez+One&display=swap, Days+One']
+    }
+  });
