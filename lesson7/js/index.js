82b7bd8fc35ff6ee225608f11cc92469484826ec
fun09002@byui.edu <fun09002@byui.edu>
2021-06-04 15:25:18 -0500

    litteral commit

--- lesson7/js/index.js
+++ lesson7/js/index.js
@@ -1,41 +1,13 @@
 
-const images = document.querySelectorAll("img[data-src]");
+const datefield = document.querySelector(".date");
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
+datefield.innerHTML = `${fulldate}`;
 
-const loadImages = (image)  => {
-  image.setAttribute('src', image.getAttribute('data-src'));
-  image.onload = () => {
-    image.removeAttribute('data-src')
-  };
-};
 
-const imgOptions = {
-  threshold: 0,
-  rootMargin: "0px 0px 200px 0px"
+const hambutton = document.querySelector(".ham");
+hambutton.addEventListener("click", toggleMenu, false);
+
+function toggleMenu() {
+  document.querySelector("nav").classList.toggle("responsive");
 }
-
-if ('IntersectionObserver' in window) {
-  const imgObserver = new IntersectionObserver(items => {
-    items.forEach(item => {
-      if (item.isIntersecting) {
-        loadImages(item.target);
-        imgObserver.unobserve(item.target);
-      }
-    });
-  }, imgOptions);
-  
-  images.forEach(img => {
-    imgObserver.observe(img);
-  });
-}
-
-else {
-  images.forEach(img => {
-    loadImages.observe(img);
-  });
-}
-
-
-
-
-
-
