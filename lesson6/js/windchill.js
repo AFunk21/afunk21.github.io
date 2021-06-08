0f52db3ab82e8656d6238e756f52b808bdc1da20
fun09002@byui.edu <fun09002@byui.edu>
2021-06-08 17:24:15 -0500

    litteral commit

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -9,4 +9,6 @@ if (parseInt(temp) <= 50 && parseInt(wind) > 3) {
     const windchill = 35.74 + 0.6215 * temp - 35.75 * Math.pow(wind, 0.16) + 0.4275 * temp * Math.pow(wind, 0.16);
     const wc_round = Math.round(windchill);  
     document.querySelector('#windchill').textContent = wc_round;
-} 
+} else {
+    document.querySelector('#windchill').textContent = "N/A";
+}
