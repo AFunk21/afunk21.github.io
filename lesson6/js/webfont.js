c7ba28c79264c44b5a9c69b0af92a5149674af39
fun09002@byui.edu <fun09002@byui.edu>
2021-06-08 17:18:20 -0500

    litteral commit

--- lesson6/js/webfont.js
+++ lesson6/js/webfont.js
@@ -1,6 +1,6 @@
 WebFont.load({
     google: {
       families: 
-        ['Roboto, Audiowide, Bree+Serif, Comfortaa:wght@600, Courgette, Girassol, Lobster, Rubik:wght@300;400, Russo+One, Suez+One&display=swap, Days+One']
+        ['Roboto, Audiowide, Courgette, Lobster, Russo+One, Days+One']
     }
   });
