d08b57d8fc9ba8dd30ee5e95e3657478b33aafd1
fun09002@byui.edu <fun09002@byui.edu>
2021-06-19 14:09:56 -0500

    litteral commit

---
+++ lesson9-home/js/storage.js
@@ -0,0 +1,23 @@
+function NumberOfDays(start, end) {
+    const oneDay = 1000 * 60 * 60 * 24;
+
+    const differenceInTime = end - start;
+
+    const diffInDays = Math.round(diffInTime / oneDay);
+
+    return diffInDays;
+}
+
+document.querySelector("span.visit").innerHTML = NumberOfDays(
+    new Date(localStorage.getItem("date")),
+    new Date()
+);
+
+localStorage.setItem("date", new Date());
+
+
+
+//codepen example code
+function adjustRating(rating) {
+    document.getElementById("ratingvalue").innerHTML = rating;
+  }
