9bf427dfae534a4aa58d02552821dcfb72c85048
fun09002@byui.edu <fun09002@byui.edu>
2021-06-19 22:14:25 -0500

    litteral commit

--- lesson9-home/js/info.js
+++ lesson9-home/js/info.js
@@ -7,25 +7,21 @@ fetch(requestURL)
     .then(function(jsonObject) {
         const towns = jsonObject["towns"];
 
-        const three = towns.filter((town) =>
-            town.name == "Preston" ||
-            town.name == "Soda Springs" ||
-            town.name == "Fish Haven"
-        );
+        const three = towns.filter((town) => town.name == "Preston" || town.name == "Soda Springs" || town.name == "Fish Haven");
 
         three.forEach((town) => {
             let town = document.createElement("section");
             let h2 = document.createElement("h2");
             let motto = document.createElement("p");
             let year = document.createElement("p");
-            let pop = document.createElement("p");
+            let population = document.createElement("p");
             let rain = document.createElement("p");
             let image = document.createElement("img");
 
             h2.textContent = town.name;
             motto.textContent = `"${town.motto}"`;
             year.textContent = `Year Founded: ${town.yearFounded}`;
-            pop.textContent = `Population: ${town.currentPopulation}`;
+            population.textContent = `Population: ${town.currentPopulation}`;
             rain.textContent = `Annual Rain Fall: ${town.averageRainfall}`;
             image.setAttribute("src", `images/${town.photo}`);
             image.setAttribute("alt", `A landscape picture of ${town.name}!`);
@@ -33,7 +29,7 @@ fetch(requestURL)
             town.appendChild(h2);
             town.appendChild(motto);
             town.appendChild(year);
-            town.appendChild(pop);
+            town.appendChild(population);
             town.appendChild(rain);
             town.appendChild(image);
 
