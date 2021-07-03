192a7cb302b69980b10f0c2fa9f8e5894874f8d2
fun09002@byui.edu <fun09002@byui.edu>
2021-07-03 09:29:53 -0500

    litteral commit

--- lesson10/js/five-day.js
+++ lesson10/js/five-day.js
@@ -6,14 +6,14 @@ const forecastURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473
 fetch(forecastURL) // from the preston weather activity
     .then((response) => response.json()) // from the preston weather activity
     .then((jsObject) => { // from the preston weather activity
-        const fiveDayForecast = jsObject.list.filter ( forecast => forecast.dt_txt.includes("18:00:00")); // "filter", "forecast.list", and "dt_txt.includes()" recomendatios from the assignment page
+        const fiveDayForecast = jsObject.list.filter (forecast => forecast.dt_txt.includes("18:00:00")); // "filter", "forecast.list", and "dt_txt.includes()" recomendatios from the assignment page
         const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']; // setting string of days
         console.log(fiveDayForecast); // from the preston weather activity
 
-        fiveDayForecast.forEach( fore_cast => {// from the preston weather activity
+        fiveDayForecast.forEach(fore_cast => {// from the preston weather activity
             const currentDate = new Date(fore_cast.dt_txt); // calling the day
             const weekDay = days[currentDate.getDay()]; // calling the day of the week
-            const temp = Math.round(forecast.main.temp_max); // calling for the temperature
+            const temp = Math.round(fore_cast.main.temp_max); // calling for the temperature
             const imagesrc = 'https://openweathermap.org/img/wn/' + fore_cast.weather[0].icon + '.png'; 
 
             let dayContainer = document.createElement('div'); //referencing structure from my week 9 assingment - info.js file 
