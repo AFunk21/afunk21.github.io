4d931d9284afa0afd333b2233c7191ebf28181be
fun09002@byui.edu <fun09002@byui.edu>
2021-07-19 22:38:16 -0500

    litteral commit

--- lesson20/js/weather-summary.js
+++ lesson20/js/weather-summary.js
@@ -1,9 +1,31 @@
-// const APPID = "7cdd4c49d24edd9b9fc476ef7cdd7ff4" 
-// const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=7cdd4c49d24edd9b9fc476ef7cdd7ff4&units=imperial";
 
 const apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=32.939861&lon=-97.078369&units=imperial&exclude=minutely&appid=7cdd4c49d24edd9b9fc476ef7cdd7ff4"
 
-fetch(apiURL) // from the weather activity
+fetch(apiURL) 
+  .then((response) => response.json())
+  .then((jsObject) => { 
+    // console.log(jsObject);
+    let onecall = jsObject 
+    let current = onecall.current.weather.main; 
+    let currentTemp = onecall.current.temp;
+    let wind = onecall.current.wind_speed;
+    let humidity = onecall.current.humidity;
+
+    // This worked last time, I am not sure why it is not working with the different API url. I included the link where I am getting this from.
+    // const mySentence = temp;
+    // const finalSentence = mySentence.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase());
+    // - link: https://www.freecodecamp.org/news/how-to-capitalize-words-in-javascript/
+
+    document.getElementById('current').textContent = current;
+    document.getElementById('temperature').textContent = currentTemp;
+    document.getElementById('wind-speed').textContent = Math.round(wind);
+    document.getElementById('humidity').textContent = humidity;
+  });
+
+
+
+// This is another attempt 
+/* fetch(apiURL) // from the weather activity
   .then((response) => response.json()) // from the weather activity
   .then((jsObject) => { // from the weather activity
     //console.log(jsObject);
@@ -13,12 +35,11 @@ fetch(apiURL) // from the weather activity
     let wind = onecall.hourly.wind_speed; 
     let humidity = onecall.hourly.humidity; 
     const mySentence = current; 
-    const description = mySentence.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase());
-    
+    const current = mySentence.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase());
 
     document.getElementById('description').textContent = current;
     document.getElementById('temperature').textContent = currentTemp;
     document.getElementById('wind-speed').textContent = Math.round(wind);
     document.getElementById('humidity').textContent = humidity;
-  });
+  }); */
 
